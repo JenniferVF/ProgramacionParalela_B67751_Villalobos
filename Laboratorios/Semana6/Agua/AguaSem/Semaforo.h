95c3e731451cd31c65db1942c5d16ee6f7cf83aa
JenniferVF <jennifervfdz@gmail.com>
2020-05-23 01:50:02 -0600

    Se agrega Laboratorio 6.

---
+++ Laboratorios/Semana6/Agua/AguaSem/Semaforo.h
@@ -0,0 +1,17 @@
+/*
+   C++ class to encapsulate Unix semaphore intrinsic structures and
+   system calls
+*/
+
+#define KEY 0xA12345	// Valor de la llave del recurso
+
+class Semaforo {
+   public:
+      Semaforo( int ValorInicial = 1 );
+      ~Semaforo();
+      int Signal();	// Puede llamarse V
+      int Wait();	// Puede llamarse P
+
+   private:
+      int id;		// Identificador del semaforo
+};
